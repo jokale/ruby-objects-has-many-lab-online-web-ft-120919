db0bd1be6dbebb08c82048cc6a92a64b1073c255
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 13:01:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,10 +15,10 @@ end
  
 
    
-  def add_song_by_name(name)
-      song = Song.new(name)
-    add_song(song)
-  end 
+  # def add_song_by_name(name)
+  #     song = Song.new(name)
+  #   add_song(song)
+  # end 
 
    
  def song_count 
