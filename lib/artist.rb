b831d00e96309145033306730207833767a0594a
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 13:01:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,12 +15,11 @@ end
  
 
    
-#   def add_song_by_name(name)
-#       song = Song.new(name)
-#     add_song(song)
-#   end 
+  def add_song_by_name(name)
+      song = Song.new(name)
+    add_song(song)
+  end 
 
-)
    
  def song_count 
    @songs.count 
