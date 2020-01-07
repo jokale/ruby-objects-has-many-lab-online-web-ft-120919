82c58ecb9de0b2a6911abd88645ededae76330bd
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 13:32:57 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,9 @@
 class Post 
-    attr_accessor :name, :artist 
+    attr_accessor :title, :author
     @@all = []
-  def initialize(name)
-    @name = name
+    
+  def initialize(title)
+    @title = title
     @@all << self
   end 
  
