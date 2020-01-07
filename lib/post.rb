cf53108d995b5ea12beeae3b9239bc1c258f4b5c
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 13:26:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,14 @@
 class Post 
-  
+    attr_accessor :name, :artist 
+    @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+ 
+ def self.all 
+   @@all 
+  end
   
 end 
   
